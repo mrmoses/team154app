690ee1aafdad571d132e4fe007ba69d55f378b38
Joe Moses <jnmoses@gmail.com>
2024-04-17 11:34:15 -0500

    fixes data file name typo

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import app_util as app_util
 import pickle
 
 # load data
-df = pd.read_csv(f'./data/small_samlpe_data.csv')
+df = pd.read_csv('./data/small_sample_data.csv')
 
 # split data into decades
 decades_dfs = app_util.get_decades_data(df)
